19eac6977195732fb33b263a417db73bd766a628
Yong Tang <yong.tang.github@outlook.com>
2025-03-06 14:07:07 -0800

    Add DebugText

--- Assets/HandTrackingSender.cs
+++ Assets/HandTrackingSender.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class HandTrackingSender : MonoBehaviour
 {
+    public TextMeshProUGUI debugText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        debugText.text = "Start";
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        debugText.text = "Update";
         
     }
 }
