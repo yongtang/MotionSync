4a54f7af03997aab78037a5558846af46033b1f9
Yong Tang <yong.tang.github@outlook.com>
2025-03-06 14:40:45 -0800

    Hand Tracking

--- Assets/HandTrackingSender.cs
+++ Assets/HandTrackingSender.cs
@@ -1,21 +1,58 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.Hands;
+using UnityEngine.XR.Management;
 
-public class HandTrackingSender : MonoBehaviour
+public class HandTrackingDisplay : MonoBehaviour
 {
-    public TextMeshProUGUI debugText;
+    public TextMeshProUGUI debugText; // UI Text in Meta Quest
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private XRHandSubsystem handSubsystem;
+
     void Start()
     {
-        debugText.text = "Start";
-        
+        // Get the Hand Tracking Subsystem
+        var loader = XRGeneralSettings.Instance?.Manager?.activeLoader;
+        if (loader != null)
+        {
+            handSubsystem = loader.GetLoadedSubsystem<XRHandSubsystem>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        debugText.text = "Update";
-        
+        if (handSubsystem != null)
+        {
+            XRHand leftHand = handSubsystem.leftHand;
+            XRHand rightHand = handSubsystem.rightHand;
+
+            // Build text message
+            string message = "Hand Tracking Active\n";
+
+            if (leftHand.isTracked)
+            {
+                message += $"Left Hand Position: {leftHand.rootPose.position}\n";
+            }
+            else
+            {
+                message += "Left Hand Not Tracked\n";
+            }
+
+            if (rightHand.isTracked)
+            {
+                message += $"Right Hand Position: {rightHand.rootPose.position}\n";
+            }
+            else
+            {
+                message += "Right Hand Not Tracked\n";
+            }
+
+            // Update UI Text
+            debugText.text = message;
+        }
+        else
+        {
+            debugText.text = "Hand Tracking Subsystem Not Found!";
+        }
     }
 }
