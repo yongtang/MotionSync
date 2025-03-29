def1a6dc578a3e12bf095f61ee849175a61d68d4
Yong Tang <yong.tang.github@outlook.com>
2025-03-29 16:00:13 -0700

    Update

--- Assets/WebSocket.cs
+++ Assets/WebSocket.cs
@@ -1,16 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class WebSocket : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+using NativeWebSocket;
 
-    // Update is called once per frame
-    void Update()
+public class Connection : MonoBehaviour
+{
+  WebSocket websocket;
+
+  // Start is called before the first frame update
+  async void Start()
+  {
+    websocket = new WebSocket("wss://service.zenimotion.com/nats");
+
+    websocket.OnOpen += () =>
     {
-        
+      Debug.Log("Connection open!");
+    };
+
+    websocket.OnError += (e) =>
+    {
+      Debug.Log("Error! " + e);
+    };
+
+    websocket.OnClose += (e) =>
+    {
+      Debug.Log("Connection closed!");
+    };
+
+    websocket.OnMessage += (bytes) =>
+    {
+      Debug.Log("OnMessage!");
+      Debug.Log(bytes);
+
+      // getting the message as a string
+      // var message = System.Text.Encoding.UTF8.GetString(bytes);
+      // Debug.Log("OnMessage! " + message);
+    };
+
+    // Keep sending messages at every 0.3s
+    InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
+
+    // waiting for messages
+    await websocket.Connect();
+  }
+
+  void Update()
+  {
+    #if !UNITY_WEBGL || UNITY_EDITOR
+      websocket.DispatchMessageQueue();
+    #endif
+  }
+
+  async void SendWebSocketMessage()
+  {
+    if (websocket.State == WebSocketState.Open)
+    {
+      // Sending bytes
+      // await websocket.Send(new byte[] { 10, 20, 30 });
+
+      // Sending plain text
+      await websocket.SendText("PUB test.subject 5\r\nHello\r\n");
     }
+  }
+
+  private async void OnApplicationQuit()
+  {
+    await websocket.Close();
+  }
+
 }
