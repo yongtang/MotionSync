219cc5c4fb7a743022228d2da7eb2b899a65253c
Yong Tang <yong.tang.github@outlook.com>
2025-04-13 21:36:03 -0700

    Track

--- Assets/LiveKitRenderer.cs
+++ Assets/LiveKitRenderer.cs
@@ -32,18 +32,27 @@ public class LiveKitRenderer : MonoBehaviour
 
     void TrackSubscribed(IRemoteTrack track, RemoteTrackPublication publication, RemoteParticipant participant)
     {
-        Debug.Log("TrackSubscribed: " + track?.Sid);
+        Debug.Log("TrackSubscribed: " + track?.Sid + "|" + participant.Identity + "|" + participant.Name);
+        if ((participant.Identity == "bot") && (track is RemoteVideoTrack videoTrack))
+        {
+            Debug.Log("Subscribed to video track: " + videoTrack.Sid);
+        }
     }
 
     void TrackUnsubscribed(IRemoteTrack track, RemoteTrackPublication publication, RemoteParticipant participant)
     {
-        Debug.Log("TrackUnsubscribed: " + track?.Sid);
+        Debug.Log("TrackUnsubscribed: " + track?.Sid + "|" + participant.Identity + "|" + participant.Name);
     }
 
     IEnumerator ConnectToRoom(string serve, string token)
     {
-        Debug.Log("ConnectToRoom");
-        yield return null;
+        Debug.Log("ConnectToRoom: " + serve + "|" + token);
+        var connect = room.Connect(serve, token, new LiveKit.RoomOptions());
+        yield return connect;
+        if (!connect.IsError)
+        {
+            Debug.Log("Connected: " + room.Name);
+        }
     }
 
     public void Connect(string serve, string token)
